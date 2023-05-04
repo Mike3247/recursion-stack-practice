c2de444efc3f5e871e9d79ea3dcebdcede3d29f2
Mike Selmani <mike_selmani@hotmail.com>
2023-05-04 19:33:52 -0400

    codingame recursion exercises # 7, totalIntegers

--- script2.js
+++ script2.js
@@ -97,3 +97,29 @@ const nestedObject = {
 const hasIt = contains(nestedObject, 44); // true
 const doesntHaveIt = contains(nestedObject, "foo"); // false
 console.log(hasIt, doesntHaveIt);
+
+// Question 7: Parse a multi-dimensional array:
+// Given a multi-dimensional integer array, return
+// the total number of integers stored inside this array
+
+function totalIntegers(array){
+	if(array.length === 0) return 0;
+
+	let total = 0;
+	const first = array.shift();
+
+	if (Array.isArray(first)){
+		total += totalIntegers(first); 
+	} else if (Number.isInteger(first)) {
+		total += 1;
+	}
+
+	return total + totalIntegers(array);
+}
+
+const zero = totalIntegers([]);
+console.log(zero);
+const three = totalIntegers([[1, 2, 4], ["foo"], [3]]);
+console.log(three);
+const seven = totalIntegers([[[5], 3], 0, 2, ["foo"], [], [4, [5, 6]]]); // 7
+console.log(seven);
