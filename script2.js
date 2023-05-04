aa444a31fd03bcc73f6b1cecd35b99c031743a1b
Mike Selmani <mike_selmani@hotmail.com>
2023-05-04 18:51:52 -0400

    codingame recursion practice exercises # 5 + # 6

--- script2.js
+++ script2.js
@@ -29,13 +29,10 @@ function factorial(num) {
     return num * factorial(num - 1); // pending multiplier
 
 }
-
-// Check all values in an array
-
+// 4. Check all values in an array
 const allAreLessThanSeven = all([1,2,9], function(num){
 	return num < 7;
 });
-
 console.log(allAreLessThanSeven); // false
 
 function all(array, callback){
@@ -50,5 +47,53 @@ function all(array, callback){
 		return false;
 	
 }
+// 5. Product of an array
 
+// Write a function called productOfArray which takes in an array
+// of numbers and returns the product of them all
 
+function productOfArray(array) {
+	if (array.length === 0) {
+		return 1;
+	}
+	return array.shift() * productOfArray(array);
+}
+const six = productOfArray([1,2,3]);
+const sixty = productOfArray([1,2,3,10]);
+console.log(six, sixty);
+console.log(productOfArray([]));
+
+// Question 6: Search JS object
+// Write a function called contains that searches for a value
+// in a nested object. It returns true if the object contains
+// that value.
+
+function contains(obj, value) {
+	for(const key in obj){
+		if(typeof obj[key] === "object"){
+			return contains(obj[key], value);
+		}if (obj[key] === value){
+			return true;
+		}
+	}
+	return false;
+}
+
+const nestedObject = {
+    data: {
+        info: {
+            stuff: {
+                thing: {
+                    moreStuff: {
+                        magicNumber: 44,
+                        something: "foo2"
+                    }
+                }
+            }
+        }
+    }
+}
+
+const hasIt = contains(nestedObject, 44); // true
+const doesntHaveIt = contains(nestedObject, "foo"); // false
+console.log(hasIt, doesntHaveIt);
